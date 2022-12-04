2a1ca4f61254151cc527c8806397c0aa5f30c994
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-04 17:53:02 +0700

    Implement nth fibo generator
    I got this warning
    warning: value assigned to `c` is never read
      --> src/main.rs:20:13
       |
    20 |     let mut c = 0;
       |             ^
       |
       = note: `#[warn(unused_assignments)]` on by default
       = help: maybe it is overwritten before being read?

--- nth_fibo_generator/src/main.rs
+++ nth_fibo_generator/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("How many fibo do you want?");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let counter: u32 = input.trim().parse().unwrap();
+    for i in 0..counter {
+        println!("The {i} fibo is {}", nth_fibo(i));
+    }
+
+}
+
+fn nth_fibo(n: u32) -> i32 {
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+    if n == 0 {
+        return 0
+    }
+    for _i in 2..=n {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    b
 }
