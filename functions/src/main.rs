aedca7b7be1b6e873363115a27827b90a2d7b96b
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-04 15:01:26 +0700

    Function return value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     println!("Hello, world!");
     another_function();
     parameterized_function(5);
+
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
 }
 
 fn another_function() {
@@ -10,4 +13,8 @@ fn another_function() {
 
 fn parameterized_function(x: u32) {
     println!("Hi I'm the other function with param value is {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
