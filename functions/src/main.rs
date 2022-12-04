30ffc199eff9b756c807e2b4b633d1f517b99175
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-04 14:53:11 +0700

    Function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    another_function();
+    parameterized_function(5);
+}
+
+fn another_function() {
+    println!("Hi I'm the other function")
+}
+
+fn parameterized_function(x: u32) {
+    println!("Hi I'm the other function with param value is {x}");
 }
