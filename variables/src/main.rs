1e8893baf7c2d043d864b34c358b5a64ffeb03cb
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-04 14:22:07 +0700

    Shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,5 +7,13 @@ fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("Three hours in seconds is: {THREE_HOURS_IN_SECONDS}");
 
-    
+    let y: u8 = 5;
+    println!("The value of y is {y}");
+    let y: u8 = 6;
+    println!("The value of y is {y}");
+    {
+        let y: f32 = 10.0;
+        println!("The value of y is {:.1}", y);
+    }
+    println!("The value of y is {y}");
 }
