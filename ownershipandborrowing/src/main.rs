bad6585fea44c532a4322320bde495e654dd66b5
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 10:23:42 +0700

    Change value from borrowed variable
    We can't change the value from next variable because next is appointed to a memory address

--- ownershipandborrowing/src/main.rs
+++ ownershipandborrowing/src/main.rs
@@ -5,7 +5,8 @@ fn main() {
     println!("\noriginal: \t\"{}\"\n", original);
 
     {
-        let next = &original;
+        let next = &mut original;
+        next = String::from("next value");
         println!("inner scope original: \t\"{}\"\n", original);
         println!("inner scope next: \t\"{}\"\n", next);
     }
