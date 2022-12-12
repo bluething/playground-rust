7bf70d1978a6548788a61485d5ce64cbd7f3c6f6
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 10:40:28 +0700

    Dangling reference
    When we want to access outer_scope which hold reference to inner_scope that has been gone (out of scope)
    Dangling reference is when the variable pointing to some data but we don't know what the data is.

--- ownershipandborrowing/src/main.rs
+++ ownershipandborrowing/src/main.rs
@@ -12,4 +12,12 @@ fn main() {
     }
 
     println!("outer scope original: \t\"{}\"\n", original);
+
+    let outer_scope: &i32;
+    {
+        let inner_scope = 5;
+        outer_scope = &inner_scope;
+    }
+
+    println!("{}", outer_scope);
 }
