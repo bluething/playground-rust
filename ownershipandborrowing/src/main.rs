59875a306a05656507d2fa2831b7e83c9a381819
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 10:47:26 +0700

    Lifetime sytax in function
    Define a lifetime as <'a, 'b>, only for variable pass by reference

--- ownershipandborrowing/src/main.rs
+++ ownershipandborrowing/src/main.rs
@@ -21,10 +21,14 @@ fn main() {
 
     println!("{}", outer_scope);
 
-    let returned_ref = return_bad_ref();
+    //let returned_ref = return_bad_ref();
 }
 
 fn return_bad_ref() -> &i32 {
     let value: i32 = 5;
     &value
+}
+
+fn lifetime_syntax<'a, 'b>(p1: &'a i32, p2: i32, p3: &'b : f64) {
+
 }
