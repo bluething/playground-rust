5a21c73279af7390345fa91a560c31a51a67a3e3
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 10:07:41 +0700

    Mutable borrowed variable
    Is ok if we change the value before we borrow the ownership

--- ownershipandborrowing/src/main.rs
+++ ownershipandborrowing/src/main.rs
@@ -1,8 +1,9 @@
 #![allow(unused)]
 
 fn main() {
-    let original: String = String::from("original value");
+    let mut original: String = String::from("original value");
     println!("\noriginal: \t\"{}\"\n", original);
+    original = String::from("new value");
 
     let next = &original;
     println!("\noriginal: \t\"{}\"\n", original);
