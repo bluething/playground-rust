f4b904163712872cd202c2dbb8fdfbaa18440c5a
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 10:16:22 +0700

    Mutable borrowed variable
    Change the original value after we borrow the ownership.
    It should be error (in pluralsight video). But I can run the code.

--- ownershipandborrowing/src/main.rs
+++ ownershipandborrowing/src/main.rs
@@ -3,8 +3,8 @@
 fn main() {
     let mut original: String = String::from("original value");
     println!("\noriginal: \t\"{}\"\n", original);
-    original = String::from("new value");
 
     let next = &original;
+    original = String::from("new value");
     println!("\noriginal: \t\"{}\"\n", original);
 }
