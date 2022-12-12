53e685055b69beff8a4fb957a46def0956da7f58
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 10:20:24 +0700

    Scope
    Move next to inner scope. After end curly brace the next variable enter the end life and destroyed.

--- ownershipandborrowing/src/main.rs
+++ ownershipandborrowing/src/main.rs
@@ -4,7 +4,11 @@ fn main() {
     let mut original: String = String::from("original value");
     println!("\noriginal: \t\"{}\"\n", original);
 
-    let next = &original;
-    original = String::from("new value");
-    println!("\noriginal: \t\"{}\"\n", original);
+    {
+        let next = &original;
+        println!("inner scope original: \t\"{}\"\n", original);
+        println!("inner scope next: \t\"{}\"\n", next);
+    }
+
+    println!("outer scope original: \t\"{}\"\n", original);
 }
