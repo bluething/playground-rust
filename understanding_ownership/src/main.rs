d02beaf863099df4d7af7001ad0d0763a415a6d3
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 14:52:28 +0700

    Return Values and Scope
    Returning values can also transfer ownership.
    The ownership of a variable follows the same pattern every time: assigning a value to another variable moves it.
    When a variable that includes data on the heap goes out of scope, the value will be cleaned up by drop unless ownership of the data has been moved to another variable.

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -26,7 +26,23 @@ fn main() {
     makes_copy(x);                  // x would move into the function,
     // but i32 is Copy, so it's okay to still
     // use x afterward
+
+    let s5 = gives_ownership();         // gives_ownership moves its return
+    // value into s5
+
+    let s6 = String::from("hello");     // s6 comes into scope
+
+    let s7 = takes_and_gives_back(s6);  // s6 is moved into
+    // takes_and_gives_back, which also
+    // moves its return value into s7
+    //println!("{}", s6);                       //borrow of moved value: s6
+    println!("{}", s7);
 }
+// Here, x goes out of scope, then s. But because s's value was moved, nothing
+// special happens.
+//
+// Here, s7 goes out of scope and is dropped. s6 was moved, so nothing
+// happens. s5 goes out of scope and is dropped.
 
 fn takes_ownership(some_string: String) { // some_string comes into scope
     println!("{}", some_string);
@@ -35,4 +51,22 @@ fn takes_ownership(some_string: String) { // some_string comes into scope
 
 fn makes_copy(some_integer: i32) { // some_integer comes into scope
     println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+    // return value into the function
+    // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+    // moves out to the calling
+    // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+    // scope
+
+    a_string  // a_string is returned and moves out to the calling function
 }
