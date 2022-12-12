7f1facbbadf09e95d3013f392d0363c897f842d6
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 14:46:06 +0700

    Ownership and Functions
    Passing a variable to a function will move or copy, just as assignment does

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -15,4 +15,24 @@ fn main() {
     let y = x;
 
     println!("x = {}, y = {}", x, y);
+
+    let s = String::from("hello");    // s comes into scope
+
+    takes_ownership(s);              // s's value moves into the function...
+    //println!("{}", s);                        // ... and so is no longer valid here
+
+    let x = 5;                             // x comes into scope
+
+    makes_copy(x);                  // x would move into the function,
+    // but i32 is Copy, so it's okay to still
+    // use x afterward
+}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+// memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
 }
