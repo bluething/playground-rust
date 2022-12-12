59977f305498a69e2533532b03d6f2f780eb58b2
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 14:38:56 +0700

    Clone
    Rust deep copy. The heap data get copied together with the pointer, length, and capacity

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -1,6 +1,13 @@
+#![allow(unused)]
+
 fn main() {
     let s1 = String::from("hello");
     let s2 = s1;
 
-    println!("{}, world!", s1);
+    //println!("{}, world!", s1);
+
+    let s3 = String::from("hello");
+    let s4 = s3.clone();
+
+    println!("s3 = {}, s4 = {}", s3, s4);
 }
