d756db00fb7ad21973852fa0d7b8349dabff8150
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-12 10:05:36 +0700

    Borrowing
    Instead of give the ownership the next variable borrow the ownership of original variable

--- ownershipandborrowing/src/main.rs
+++ ownershipandborrowing/src/main.rs
@@ -1,7 +1,9 @@
+#![allow(unused)]
+
 fn main() {
     let original: String = String::from("original value");
     println!("\noriginal: \t\"{}\"\n", original);
 
-    let next = original;
-    println!("{}", original);
+    let next = &original;
+    println!("\noriginal: \t\"{}\"\n", original);
 }
