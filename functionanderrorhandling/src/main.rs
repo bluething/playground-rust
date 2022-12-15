8bb00b1c2663154af1e8a07deb74cd3b5b1c8ef6
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-15 14:40:44 +0700

    Function parameter pass by mutable reference

--- functionanderrorhandling/src/main.rs
+++ functionanderrorhandling/src/main.rs
@@ -1,14 +1,21 @@
 #![allow(unused)]
 
+use std::io::Stderr;
+
 fn main() {
     let greater = return_greater(10, 5);
     println!("{}", greater);
 
     let mut original = String::from("original value");
     println!("\nouter scope original: \t\"{}\"", original);
+
     {
         print_original(&original);
+        change_original(&mut original);
+        println!("inner scope original: \t\"{}\"", original);
     }
+
+    println!("outer scope original: \t\"{}\"", original);
 }
 
 fn return_greater(first: u8, second: u8) -> u8 {
@@ -21,4 +28,10 @@ fn return_greater(first: u8, second: u8) -> u8 {
 
 fn print_original(original: &String) {
     println!("fn print_original: \t\"{}\"", original);
+}
+
+fn change_original(original: &mut String) {
+    let next = original;
+    *next = String::from("next value");
+    println!("fn change_original: \t\"{}\"", next);     // use next because original no longer own the memory
 }
