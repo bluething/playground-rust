edd5d8406bd08fedd3b4a855c333735811e5ac98
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-15 14:27:41 +0700

    Simple function with parameter

--- functionanderrorhandling/src/main.rs
+++ functionanderrorhandling/src/main.rs
@@ -1,3 +1,14 @@
+#![allow(unused)]
+
 fn main() {
-    println!("Hello, world!");
+    let greater = return_greater(10, 5);
+    println!("{}", greater);
+}
+
+fn return_greater(first: u8, second: u8) -> u8 {
+    if first > second {
+        first
+    } else {
+        second
+    }
 }
