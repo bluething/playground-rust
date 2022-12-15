37daa61728e863d108ef669773d844d6875bed82
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-15 14:35:16 +0700

    Function parameter pass by reference

--- functionanderrorhandling/src/main.rs
+++ functionanderrorhandling/src/main.rs
@@ -3,6 +3,12 @@
 fn main() {
     let greater = return_greater(10, 5);
     println!("{}", greater);
+
+    let mut original = String::from("original value");
+    println!("\nouter scope original: \t\"{}\"", original);
+    {
+        print_original(&original);
+    }
 }
 
 fn return_greater(first: u8, second: u8) -> u8 {
@@ -11,4 +17,8 @@ fn return_greater(first: u8, second: u8) -> u8 {
     } else {
         second
     }
+}
+
+fn print_original(original: &String) {
+    println!("fn print_original: \t\"{}\"", original);
 }
