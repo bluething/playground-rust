93e3071056f44d1fcdcc91e1f2dee5d3981f472f
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-16 16:52:16 +0700

    area() using struct
    The function have one parameter and the type is an immutable borrow of a struct Rectangle instance.
    Why we borrow instead of take the ownership? Because if we borrow the reference the main() can retains its ownership and can continue using rect2

--- using_struct/src/main.rs
+++ using_struct/src/main.rs
@@ -59,6 +59,16 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area_tuple(rect1)
     );
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&rect2)
+    );
 }
 
 struct User {
@@ -97,4 +107,12 @@ fn area(width: u32, height: u32) -> u32 {
 
 fn area_tuple(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
