822e5576994516e6279b1cdbd1df0a5d01e4fb17
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-16 16:13:25 +0700

    Return struct instance in the function body

--- using_struct/src/main.rs
+++ using_struct/src/main.rs
@@ -16,6 +16,9 @@ fn main() {
     };
     user2.email = String::from("fulan@example.com");
     println!("User email: {}", user2.email);
+
+    let user3 = build_user(String::from("fulan@example.com"), String::from("Fulan"));
+    println!("User name: {}", user3.username);
 }
 
 struct User {
@@ -23,4 +26,13 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
