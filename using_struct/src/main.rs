0f23e63aba4a2ee861a7ac0d9e8c1888317d44b3
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-16 16:47:39 +0700

    area() using tuple
    The code more clean (using single parameter) but still less readable because tuples don’t name their elements, so we have to index into the parts of the tuple, making our calculation less obvious.

--- using_struct/src/main.rs
+++ using_struct/src/main.rs
@@ -53,6 +53,12 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     );
+
+    let rect1 = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_tuple(rect1)
+    );
 }
 
 struct User {
@@ -87,4 +93,8 @@ struct AlwaysEqual;
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
