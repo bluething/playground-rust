200409b79bc989c4f019da8caf182c00d6ec26cf
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-16 16:06:35 +0700

    Define struct and access the field

--- using_struct/src/main.rs
+++ using_struct/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("User email: {}", user1.email);
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
