cb97dc6ad50c7b33c38560c3ca43412386d829c6
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-18 17:03:29 +0700

    Using enum over null values
    enum Option<T> {
        None,
        Some(T),
    }

--- enum_and_pattern_matching/src/main.rs
+++ enum_and_pattern_matching/src/main.rs
@@ -1,6 +1,16 @@
+#![allow(unused)]
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+
+    println!("{}", some_number.unwrap());
+    //println!("{}", absent_number.unwrap());     // casue panic
 }
 
 enum IpAddrKind {
