fee079bd9456508c15e9d4af813017b7ec74b2d6
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-18 17:25:11 +0700

    Match are exhaustive
    Must catch all pattern

--- enum_and_pattern_matching/src/main.rs
+++ enum_and_pattern_matching/src/main.rs
@@ -28,6 +28,13 @@ fn main() {
     let six = plus_one(five);
     println!("{}", six.unwrap());
     let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),          // or _ => (), that means nothing else happen
+    }
 }
 
 enum IpAddrKind {
@@ -67,3 +74,7 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
         Some(i) => Some(i + 1),
     }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn reroll() {}
