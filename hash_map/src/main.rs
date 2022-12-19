b5d2d41112ebb6c90f7a57f27bfd3ec71578c30b
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-19 15:51:05 +0700

    Updating a Value Based on the Old Value
    Counts how many times each word appears in some text. We use a hash map with the words as keys and increment the value to keep track of how many times we’ve seen that word.

--- hash_map/src/main.rs
+++ hash_map/src/main.rs
@@ -31,4 +31,15 @@ fn main() {
 
     println!("{:?}", scores);
 
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
 }
