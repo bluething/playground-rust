1c4ebaed5d329c6000a0bd9cf7f4c06b2d8d3e56
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-19 09:39:06 +0700

    Starting Relative Paths with super
    We use super to go to the parent module of back_of_house, which in this case is crate, the root.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -21,6 +21,16 @@ pub fn eat_at_restaurant() {
     front_of_house::hosting::add_to_waitlist();
 }
 
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+    fn cook_order() {}
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
