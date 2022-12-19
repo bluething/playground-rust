360dc8fa8ddd6212418628c9790ae66346bc8fa8
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-19 09:58:44 +0700

    Bringing Paths into Scope with the use Keyword
    Adding use and a path in a scope is similar to creating a symbolic link in the filesystem. Only valid on the same scope.
    Paths brought into scope with use also check privacy, like any other paths.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,9 +14,11 @@ mod front_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
     // relative path
     front_of_house::hosting::add_to_waitlist();
 
