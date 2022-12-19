9e69e032f8bcaad5c5ebaa7763de17b13bbb8eca
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-19 14:00:59 +0700

    Vector storre multiple type
    Use enum if we want to store different type in vector

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -32,4 +32,20 @@ fn main() {
         println!("{i}");
     }
 
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    for spreadsheetcel in row {
+        println!("{:?}", spreadsheetcel);
+    }
+
 }
