510f2f50ab38be4e0ec7d2a1eea343bc58f53369
Habib Ali Machpud <habib.machpud@gmail.com>
2022-12-19 13:48:22 +0700

    Accessing vector
    There are two ways to reference a value stored in a vector:
    - Via indexing (fail fast)
    - Using the get method (fail safe)
    Why Rust give us 2 methods? We have choices how the program behaves when you try to use an index value outside the range of existing elements.
    The different is using get the Rust won't panic, so, we have a choice how to handle None.

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
     let v: Vec<i32> = Vec::new();
-    let v = vec![1, 2, 3];
+    let v2 = vec![1, 2, 3];
+
+    let v3 = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v3[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v3.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
 }
